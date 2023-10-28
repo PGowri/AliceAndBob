6aa142aa061a5f90e0c056e792d34bc39435ea5f
Gowri <gowri@ZipcodersMBP64.local>
2023-10-28 14:01:15 -0400

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+       // String name1 = "Alice";
+        // String name2 = "Bob";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        if (name.equals("Alice") || name.equals("Bob")) {
+            System.out.print("Hello " + name+ "!");
+        } else {
+            System.out.print("Your name is not \"Alice\" or \"Bob\"" );
+        }
 
     }
 }
